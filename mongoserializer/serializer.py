cdc4aef9c28ba85209b16d2cbca93c009edfefb9
pioclemente <pioclemente@gmail.com>
2012-12-01 11:05:04 +0100

    fix import

--- mongoserializer/serializer.py
+++ mongoserializer/serializer.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from pymongo.dbref import DBRef
-#from pymongo.objectid import ObjectId
+from bson.dbref import DBRef
 from bson.objectid import ObjectId
 import datetime
 from pprint import pprint as pp
